d835b3700923e50b3ab957e32212e3db791ff498
netesh paudel <netesh.paudel@gmail.com>
2021-11-18 23:04:21 +0545

    singly linked list

---
+++ implementation of singly linked list.cpp
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct SLL{
+	int data;
+	struct SLL *next;	
+
+};
+struct SLL *head=NULL;
+void addatbegining(){
+	struct SLL *newnode;
+	int num;
+	printf("Enter the number : ");
+	scanf("%d",&num);
+	newnode=(struct SLL*)malloc(sizeof(struct SLL));
+	newnode->data=num;
+	newnode->next=NULL;
+	if(head==NULL){
+		head=newnode;
+	}else{
+		newnode->next=head;
+		head=newnode;
+	}
+}
+void display(){
+	struct SLL *temp;
+	if(head==NULL){
+		printf("\nLinked list doesnot exist");
+	}else{
+		temp=head;
+		while(temp!=NULL){
+			printf("%d->",temp->data);
+			temp=temp->next;
+		}
+	}
+}
+void addat end(){
+	struct SLL *newnode;
+	int num;
+	printf("Enter the number : ");
+	scanf("%d",&num);
+	newnode=(struct SLL*)malloc(sizeof(struct SLL));
+	newnode->data=num;
+	newnode->next=NULL;
+	if(head==NULL){
+		head=newnode;
+	}else{
+		temp=head;
+	}
+	while(temp->next!=NULL)
+}
+int main(){
+	int choice;
+	printf("1. Add begining \n2. Display \n3. Add at end \n4. exit");
+	while(1){
+		printf("\nEnter your choice : ");
+		scanf("%d",&choice);
+		switch(choice){
+			case 1:
+				addatbegining();
+				break;
+			case 2:
+				display();
+				break;
+			case 3:
+				addat end();
+			case 4:
+				exit(0);	
+			default:
+				printf("\nEnter valid number");
+				break;			
+		}
+	}
+	
+	
+	return 0;
+}
